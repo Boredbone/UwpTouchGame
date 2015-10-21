d41bc9c0ecadd5cd44281586611e70d04e4f92ea
boredbone <Boredbone@users.noreply.github.com>
2015-10-21 14:34:48 +0900

    Initial Commit

---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boredbone.XamlTools.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public bool IsDisposed => this.disposables.IsDisposed;
+
+        private CompositeDisposable disposables = new CompositeDisposable();
+        protected CompositeDisposable Disposables => this.disposables;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void Dispose()
+        {
+            this.disposables.Dispose();
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            var d = PropertyChanged;
+            if (d != null)
+                d(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
