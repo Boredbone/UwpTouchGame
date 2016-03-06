11e53d9a02a84a0d7dd2bd582c3514f17acea9b8
boredbone <Boredbone@users.noreply.github.com>
2016-03-06 22:42:39 +0900

    LineTraceをMarkerManager内に移動

--- UwpTouchGame/Models/MarkerManager.cs
+++ UwpTouchGame/Models/MarkerManager.cs
@@ -67,5 +67,43 @@ namespace UwpTouchGame.Models
         }
 
         public IEnumerable<MarkerContainer> GetAll() => this.Markers.AsEnumerable();
+
+
+        /// <summary>
+        /// Provide a completion event of line trace 
+        /// </summary>
+        private class LineTrace : DisposableBase
+        {
+            private Subject<LineNode> LineCompletedSubject { get; }
+            public IObservable<LineNode> LineCompleted => this.LineCompletedSubject.AsObservable();
+
+
+            public LineTrace(IObservable<MarkerContainer> hitObservable)
+            {
+                this.LineCompletedSubject = new Subject<LineNode>().AddTo(this.Disposables);
+
+                var hitMarker = hitObservable
+                    .Select(x => x.Marker)
+                    .Publish()
+                    .RefCount();
+
+                // Line Hit
+                var lineObservable = hitMarker
+                    .Where(x => x.Type == MarkerType.Node || x.Type == MarkerType.Line)
+                    .Select(x => (ILine)x)
+                    .Publish()
+                    .RefCount();
+
+                // Node Hit
+                hitMarker
+                    .Where(x => x.Type == MarkerType.Node)
+                    .Select(x => (LineNode)x)
+                    .Do(x => x.Capture(lineObservable))
+                    .Where(x => x.Parent?.IsCaptured ?? false)
+                    .Subscribe(this.LineCompletedSubject)
+                    .AddTo(this.Disposables);
+            }
+
+        }
     }
 }
